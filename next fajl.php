77d840fd793eebbca098ad7e4f6a9b8ecada7c01
deimon <deimon@localhost>
2015-03-25 19:18:06 +0100

    tfuj na tebe

--- next fajl.php
+++ next fajl.php
@@ -2,18 +2,14 @@
 vsechno smazu
 a ted sem zvedavej co na to bude rikat muj klient :D
 nooooo :-)
-    
-    
     obcas mi sem neco napis ... zmackniiiiii Team > commit .. napis nahoru nejakou kravinu > commit 
         a pak Team > remote > push .. .nexte next fiuniš
-        
         rozumnela ? jednou za x minut.. bo po vyzvani ;)
-
-po vyzvanji :D
+            po vyzvanji :D
             oka
             vyzyvej :D
-            
             och ouva vagabundos 
-            
-            
             -> ultra hyper moderni toaletni misa
+         
+          
+          tfuj na tebe .. opraveno uploadovano

f14ba86a4cfbc2207d375b9abfa16b340fd08508
deimon <deimon@localhost>
2015-03-25 18:52:30 +0100

    222dfs1

---
+++ next next fajl.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this templatse file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+die();

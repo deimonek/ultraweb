d8453a26de95c2fb5e31426bd289fd78fccfc2ba
Martin <Martin@SEO>
2015-03-25 19:08:05 +0100

    rytmus pičo pank

--- next fajl.php
+++ next fajl.php
@@ -4,4 +4,11 @@ a ted sem zvedavej co na to bude rikat muj klient :D
 nooooo :-)
     
     
-    obcas mi sem neco napis ... zmackniiiiii Team > commit .. napis nahoru nejakou kravinu 
+    obcas mi sem neco napis ... zmackniiiiii Team > commit .. napis nahoru nejakou kravinu > commit 
+        a pak Team > remote > push .. .nexte next fiuniš
+        
+        rozumnela ? jednou za x minut.. bo po vyzvani ;)
+
+po vyzvanji :D
+            oka
+            vyzyvej :D

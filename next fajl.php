bca1ad261b713f5af5f4d0a411570e9bf17ed96a
Martin <Martin@SEO>
2015-03-25 19:06:44 +0100

    dsaddadsadkladnlksaada

--- next fajl.php
+++ next fajl.php
@@ -1,3 +1,7 @@
 <?php
 vsechno smazu
-
+a ted sem zvedavej co na to bude rikat muj klient :D
+nooooo :-)
+    
+    
+    obcas mi sem neco napis ... zmackniiiiii Team > commit .. napis nahoru nejakou kravinu 
